b843d103620b6302798a7ecebd1dfe7b3469dd36
durant.julio <juliocesar30708344@gmail.com>
2024-06-02 18:35:02 -0400

    add file

---
+++ python.py
@@ -0,0 +1,17 @@
+frutas=["banana", "manzana", "helado"]
+x, y, z= frutas
+print(x)
+print(y)
+print(z)
+
+edad=int(input("ingrese edad:"))
+print("tu edad es:",edad)
+
+if edad > 18:
+     print("es mayor")
+
+else:
+    print("es menor")    
+
+
+
